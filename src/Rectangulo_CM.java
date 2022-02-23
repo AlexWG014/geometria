58c79c9d782e2c44c14d65a65a9d83d11ac0a8c4
Camilo Martinez <johnmarriv@alu.edu.gva.es>
2022-02-23 11:52:16 +0100

    version comentada

--- src/Rectangulo_CM.java
+++ src/Rectangulo_CM.java
@@ -1,19 +1,37 @@
+/** 
+* @autor Camilo Martinez
+* @version 1.2
+**/
 
 public class Rectangulo_CM extends FiguraGeometrica_CM {
 	private double l1;
 	private double l2;
 	
+	
+/**
+ * Constructor.
+ * @param tipoFigura
+ * @param lG
+ * @param lP
+**/
 	public Rectangulo_CM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+/**
+ * Calcula el área.
+ * @return l1 * l2
+**/
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+/**
+* Calcula el perimetro.
+* @return l1 * l2
+**/
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
