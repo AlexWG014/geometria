8f80b85cf5e7783c8af5350a629111eb8364d693
AlexWG014 <94621277+AlexWG014@users.noreply.github.com>
2022-02-28 19:11:07 +0100

    Update and rename Rectangulo_CM.java to Rectangulo_AMM.java

--- src/Rectangulo_AMM.java
+++ src/Rectangulo_AMM.java
@@ -1,9 +1,9 @@
 /** 
-* @autor Camilo Martinez
+* @autor Alex Miralles Montalvo
 * @version 1.2
 **/
 
-public class Rectangulo_CM extends FiguraGeometrica_CM {
+public class Rectangulo_CM extends FiguraGeometrica_AMM {
 	private double l1;
 	private double l2;
 	
@@ -14,7 +14,7 @@ public class Rectangulo_CM extends FiguraGeometrica_CM {
  * @param lG
  * @param lP
 **/
-	public Rectangulo_CM(String tipoFigura, double lG, double lP) {
+	public Rectangulo_AMM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
